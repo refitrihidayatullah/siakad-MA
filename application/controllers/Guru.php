e972af65e56fd3cbbf7fd277a6c35efe6cb366e4
Arrohim Dwi Ksatria <76282186+dwiadisa@users.noreply.github.com>
2023-01-04 11:21:04 +0700

    test controller

---
+++ application/controllers/Guru.php
@@ -0,0 +1,17 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Guru extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+}
+
+/* End of file Guru.php and path \application\controllers\Guru.php */
